97c87526ec00ce9fd99d1e9e2dd8a5ab825baa44
Mohit Verma <47892740+mohitverma6161@users.noreply.github.com>
2021-04-05 23:37:43 +0530

    Completed the Blog Project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 
 import Navbar from './Navbar';
 import Home from './Home'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
 
 function App() {
 
-  const title='Welcome to the new blog';
-  const likes=50;
-  const link="http://www.google.com"
+  
   return (
+    <Router>
     <div className="App">
       <Navbar />
      <div className="content">
-       <Home/>
-       {/* <h1>App Component</h1> */}
-       {/* <h1>{title}</h1>
-       <p>Liked {likes} times</p>
-       <p>{10}</p>
-       <p>{"hello, ninjas"}</p>
-       <p>{[1,2,3,4,5]}</p>
-       <p>{Math.random()*10}</p>
-       <a href={link}>Google Site</a> */}
+       <Switch>
+         <Route exact path="/">
+          <Home/>
+          </Route>
+          <Route path="/create">
+          <Create/>
+          </Route>
+          <Route path="/blogs/:id">
+          <BlogDetails/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+       </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,21 @@
-import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  
+  const {data:blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
 
 
-const [name,setname]=useState('mario');
-  const handleDelete=(id)=>{
-      const newBlogs=blogs.filter(blog=>blog.id!==id)
-      setBlogs(newBlogs)
-  }
+ 
 
-useEffect(()=>{
-    console.log('use effect ran');
-   console.log(name)
-},[name]);
+
   return (
     <div className="home">
-     <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
-     <button onClick={()=>setname('luigi')}>Change name</button>
-     <p>{name}</p>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading..</div>}
+     {blogs&&<BlogList blogs={blogs} title="All blogs!"/>}
+     {/* <button onClick={()=>setname('luigi')}>Change name</button> */}
+    
     </div>
   );
 }
